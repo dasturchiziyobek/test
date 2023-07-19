0c4f8fd001407bbd508ac44617daac96256e0167
dasturchiziyobek <ziyobekdasturchi@gmail.com>
2023-07-19 14:22:32 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+import pyttsx3
+
+uzgarmas = ['90','91','93','94','95','97','99','88','33','77','55']
+uzgaruvchan = random.randint(0,9999999)
+uzgaruvchan1 = random.randint(0,9999999)
+uzgaruvchan2 = random.randint(0,9999999)
+uzgaruvchan3 = random.randint(0,9999999)
+uzgaruvchan4 = random.randint(0,9999999)
+uzgaruvchan5 = random.randint(0,9999999)
+
+tanlov = random.choice(uzgarmas)
+tanlov1 = random.choice(uzgarmas)
+tanlov2 = random.choice(uzgarmas)
+tanlov3 = random.choice(uzgarmas)
+tanlov4 = random.choice(uzgarmas)
+tanlov5 = random.choice(uzgarmas)
+print(tanlov,uzgaruvchan)
+print(tanlov1,uzgaruvchan1)
+print(tanlov2,uzgaruvchan2)
+print(tanlov3,uzgaruvchan3)
+print(tanlov4,uzgaruvchan4)
+print(tanlov5,uzgaruvchan5)
